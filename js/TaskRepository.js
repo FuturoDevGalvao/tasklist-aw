08c5cb6c0aa0730be7941eefdb194102a4d0942d
FuturoDevGalvao <inosuke567765@gmail.com>
2024-05-10 03:19:33 -0300

    feat: métodos parcialmente implementados

--- js/TaskRepository.js
+++ js/TaskRepository.js
@@ -1,40 +1,72 @@
-export class TaskRepository {
-  constructor() {
-    // Aqui você pode inicializar qualquer configuração necessária para se conectar ao banco de dados
-  }
+import { Task } from "./Task.js";
 
-  // Método para buscar todas as tasks no banco de dados
-  getAllTasks() {
-    // Implemente a lógica para buscar todas as tasks no banco de dados
-    // Retorne uma lista de tasks
-    return []; // Exemplo: retornando uma lista vazia por enquanto
+export class TaskRepository {
+  /**
+   *
+   * @returns retorna nulo, caso o localStorage esteja vazio
+   */
+  static getAllTasks() {
+    const keys = Object.keys(localStorage);
+    const allTasks = [];
+
+    if (keys.length === 0) return null;
+
+    keys.forEach((key) => allTasks.push(localStorage.getItem(key)));
+
+    return allTasks
+      .map((task) => JSON.parse(task))
+      .map((task) => Task.fromJSON(task));
   }
 
   // Método para buscar uma task pelo ID no banco de dados
-  getTaskById(taskId) {
+  static getTaskById(taskId) {
     // Implemente a lógica para buscar uma task pelo ID no banco de dados
     // Retorne a task encontrada ou null se não encontrada
     return null; // Exemplo: retornando null por enquanto
   }
 
+  static #getNextId() {
+    // Obter todas as chaves do localStorage
+    const keys = Object.keys(localStorage);
+
+    if (keys.length === 0) return 1;
+
+    const lastId = Number(keys[keys.length - 1]);
+    const newId = lastId + 1;
+
+    return newId;
+  }
+
   // Método para salvar uma nova task no banco de dados
-  saveTask(task) {
-    // Implemente a lógica para salvar uma nova task no banco de dados
-    // Retorne a task salva com o ID atribuído pelo banco de dados
-    return task; // Exemplo: retornando a mesma task por enquanto
+  static saveTask(task) {
+    const taskFormatedToJSON = JSON.stringify(task);
+
+    localStorage.setItem(this.#getNextId(), taskFormatedToJSON);
   }
 
   // Método para atualizar uma task existente no banco de dados
-  updateTask(task) {
+  static updateTask(task) {
     // Implemente a lógica para atualizar uma task existente no banco de dados
     // Retorne true se a atualização foi bem-sucedida ou false se ocorreu algum erro
     return true; // Exemplo: retornando true por enquanto
   }
 
+  static #getIDTask(taskToSearch) {
+    const keys = Object.keys(localStorage);
+
+    for (const key of keys) {
+      const task = Task.fromJSON(JSON.parse(localStorage.getItem(key)));
+
+      if (task.title === taskToSearch.title) {
+        console.log(key);
+        return key;
+      }
+    }
+
+    return -1;
+  }
   // Método para deletar uma task pelo ID no banco de dados
-  deleteTask(taskId) {
-    // Implemente a lógica para deletar uma task pelo ID no banco de dados
-    // Retorne true se a exclusão foi bem-sucedida ou false se ocorreu algum erro
-    return true; // Exemplo: retornando true por enquanto
+  static deleteTask(task) {
+    localStorage.removeItem(this.#getIDTask(task));
   }
 }
