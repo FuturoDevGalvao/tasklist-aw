d5932f812089ddcabe7e2d1e0a5768ba86d51d24
FuturoDevGalvao <inosuke567765@gmail.com>
2024-05-10 03:25:03 -0300

    feat: task actions implementas parcialmente

--- js/script.js
+++ js/script.js
@@ -64,6 +64,18 @@ const getTaskData = () => {
   };
 };
 
+const createTaskOfDomElement = (domElement) => {
+  const title = domElement.querySelector("#title");
+  const description = domElement.querySelector("#description");
+  const priority = document.querySelector("#priority");
+
+  return new Task(
+    title.textContent,
+    description.textContent,
+    priority.textContent
+  );
+};
+
 const createTask = (taskData) => {
   const { inputTitleValue, inputDescriptionValue, priorityValue } = taskData;
 
@@ -72,41 +84,46 @@ const createTask = (taskData) => {
 
 const getTimeCriationTask = (task) => {};
 
-const saveTask = (task) => {
-  const taskFormatedToJSON = JSON.stringify(task);
-};
-
-const setTaskCardData = (task) => {
+const createTaskCard = () => {
   const { tasksContain } = tasksContainElements;
-
-  /*   const timeOfCreation = getTimeCriationTask(task);
+  const allTasks = TaskRepository.getAllTasks();
+  /*   
+  const timeOfCreation = getTimeCriationTask(task);
   console.log(timeOfCreation);
- */
-  tasksContain.innerHTML += `
-      <div class="task">
-          <div class="header-task">
-              <div class="about-task">
-                <span id="title">${task.title}</span>
-                <span id="time-of-creation">1 day ago</span>
-              </div>
-              <span id="priority">${task.priority}</span>
-          </div>
-          <div class="body-task">
-              <span id="description">${task.description}</span>
-          </div>
-          <div class="actions">
-              <button class="btn-action" id="edit">
-                  <i class="fa-solid fa-pen-to-square"></i>
-              </button>
-              <button class="btn-action" id="delete">
-                  <i class="fa-solid fa-trash-can"></i>
-              </button>
-              <button class="btn-action" id="completed">
-                  <i class="fa-solid fa-check"></i>
-              </button>
-          </div>
-      </div>
-  `;
+  */
+  tasksContain.innerHTML = "";
+
+  if (allTasks !== null) {
+    allTasks.forEach((task) => {
+      tasksContain.innerHTML += `
+        <div class="task">
+            <div class="header-task">
+                <div class="about-task">
+                  <span id="title">${task.title}</span>
+                  <span id="time-of-creation">1 day ago</span>
+                </div>
+                <span id="priority">${task.priority}</span>
+            </div>
+            <div class="body-task">
+                <span id="description">${task.description}</span>
+            </div>
+            <div class="actions">
+                <button class="btn-action" id="edit">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button class="btn-action" id="delete">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
+                <button class="btn-action" id="completed">
+                    <i class="fa-solid fa-check"></i>
+                </button>
+            </div>
+        </div>
+        `;
+
+      addListenerButtonsAction();
+    });
+  }
 };
 
 const addTask = () => {
@@ -119,13 +136,53 @@ const addTask = () => {
 
     const task = createTask(taskData);
 
-    saveTask(task);
+    TaskRepository.saveTask(task);
 
-    //setTaskCardData(task);
+    createTaskCard();
   } else {
     addErrorStyleInputs();
     showAlert();
   }
+};
+
+const getTaskDomElement = () => {
+  return {
+    tasks: document.querySelectorAll(".task"),
+  };
+};
+
+const addListenerButtonsAction = () => {
+  const { tasks } = getTaskDomElement();
+
+  tasks.forEach((task) => {
+    task.addEventListener("click", (event) => {
+      const targetButton = event.target.closest("button");
+      if (!targetButton) return; // Saia se o clique não foi em um botão
+
+      const buttonId = targetButton.id;
+
+      // Capturar a task pai (a div com a classe "task")
+      const clickedTask = event.currentTarget;
+
+      console.log(`Botão com ID "${buttonId}" clicado na task:`);
+      console.log(targetButton);
+      console.log(clickedTask);
+
+      switch (buttonId) {
+        case "edit":
+          break;
+
+        case "delete":
+          const task = createTaskOfDomElement(clickedTask);
+          TaskRepository.deleteTask(task);
+          createTaskCard();
+          break;
+
+        case "completed":
+          break;
+      }
+    });
+  });
 };
 
 const addListenerElements = () => {
